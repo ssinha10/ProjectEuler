4b0eb2e9fbd22844d59d63ae898134b11d8e201f
Suraj Sinha <ssinha10@illinois.edu>
2016-06-08 17:34:05 -0700

    ..

---
+++ Problem15.py
@@ -0,0 +1,9 @@
+import math
+
+a = math.factorial(40)
+b = math.factorial(20)
+
+c = a/(b**2)
+
+print c
+
---
+++ Problem16.py
@@ -0,0 +1,18 @@
+import math
+
+a = math.pow(2,4)
+b = str(a)
+c = []
+
+
+sum = 0
+
+for number in b:
+	c.append(number)
+
+	
+
+for x in c:
+	sum += x
+
+print sum
